48656aea287839697006858e1b70bf48c8c8330c
Tijani Sylla <tijani.sylla1@gmail.com>
2023-05-05 00:45:18 -0500

    added new project

--- src/components/data/data.ts
+++ src/components/data/data.ts
@@ -11,6 +11,7 @@ import Fitness from "../assets/Fitness.png";
 import Task from "../assets/Task.png";
 import AI from "../assets/openai-featured.jpg";
 import Movie from "../assets/movie.png";
+import calculator2 from "../assets/calculator2.png";
 
 export const data: TypeMyData[] = [
   {
@@ -22,6 +23,14 @@ export const data: TypeMyData[] = [
   },
   {
     id: 2,
+    title: "Calculator phone app",
+    img_url: `${calculator2}`,
+    link: "https://github.com/tijanisylla/sylla-calculator",
+    gitHub: "https://github.com/tijanisylla/sylla-calculator",
+  },
+
+  {
+    id: 3,
     title: "Movie App",
     img_url: `${Movie}`,
     link: "https://tijanisylla.github.io/movie-app",
@@ -29,35 +38,35 @@ export const data: TypeMyData[] = [
   },
 
   {
-    id: 3,
+    id: 4,
     title: "Calculator",
     img_url: `${calculator}`,
     link: "https://calculator-sylla.netlify.app",
     gitHub: "https://github.com/tijanisylla",
   },
   {
-    id: 4,
+    id: 5,
     title: "Snake",
     img_url: `${Snake}`,
     link: "https://snake-sylla.netlify.app",
     gitHub: "https://github.com/tijanisylla",
   },
   {
-    id: 5,
+    id: 6,
     title: "Retro-draw",
     img_url: `${Retro}`,
     link: "https://retro-draw-sylla.netlify.app",
     gitHub: "https://github.com/tijanisylla",
   },
   {
-    id: 6,
+    id: 7,
     title: "Stranger-Things",
     img_url: `${Stranger}`,
     link: "https://stranger-things1.netlify.app",
     gitHub: "https://github.com/tijanisylla",
   },
   {
-    id: 7,
+    id: 8,
     title: "UserHub",
     img_url: `${UserHub}`,
     link: "https://userhub-js.netlify.app",
@@ -65,14 +74,14 @@ export const data: TypeMyData[] = [
   },
 
   {
-    id: 8,
+    id: 9,
     title: "Art-collector",
     img_url: `${ArtCollector}`,
     link: "https://art-collector.netlify.app",
     gitHub: "https://github.com/tijanisylla",
   },
   {
-    id: 9,
+    id: 10,
     title: "Animation",
     img_url: `${Animation}`,
     link: "https://animation-js-sylla.netlify.app",
@@ -80,14 +89,14 @@ export const data: TypeMyData[] = [
   },
 
   {
-    id: 10,
+    id: 11,
     title: "Fitness",
     img_url: `${Fitness}`,
     link: "https://fitnesstrackerr.netlify.app",
     gitHub: "https://github.com/tijanisylla",
   },
   {
-    id: 11,
+    id: 12,
     title: "Tasks Progress",
     img_url: `${Task}`,
     link: "https://main.d18m2yn7ohwgrq.amplifyapp.com/",
